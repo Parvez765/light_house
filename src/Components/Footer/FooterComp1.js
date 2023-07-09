c60399504d4e63d0a91a1ac5543d9b07e09e3adc
tantaana <shariartanvirofficial@gmail.com>
2023-07-10 03:34:11 +0600

    commit for footer

--- src/Components/Footer/FooterComp1.js
+++ src/Components/Footer/FooterComp1.js
@@ -1,4 +1,12 @@
 import React from "react";
+import logo from "../../Assets/logo.png";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsYoutube,
+  BsLinkedin,
+} from "react-icons/bs";
 
 const FooterComp1 = () => {
   return (
@@ -21,6 +29,20 @@ const FooterComp1 = () => {
           <h2 className="bg-sky-600 hover:bg-sky-800 hover:cursor-pointer text-white rounded-lg duration-300 ease-in-out text-base xl:text-lg font-semibold py-1.5 px-4">
             Get Started
           </h2>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <div className="">
+          <img src={logo} className="w-[250px]" alt="" />
+        </div>
+
+        <div className="flex items-center gap-8">
+          <BsFacebook className="text-2xl text-gray-400 hover:text-black duration-300 ease-in-out hover:cursor-pointer" />
+          <BsInstagram className="text-2xl text-gray-400 hover:text-black duration-300 ease-in-out hover:cursor-pointer" />
+          <BsTwitter className="text-2xl text-gray-400 hover:text-black duration-300 ease-in-out hover:cursor-pointer" />
+          <BsYoutube className="text-2xl text-gray-400 hover:text-black duration-300 ease-in-out hover:cursor-pointer" />
+          <BsLinkedin className="text-2xl text-gray-400 hover:text-black duration-300 ease-in-out hover:cursor-pointer" />
         </div>
       </div>
     </div>
