a23553091af10ab896232a1d70556b4060e801b9
Pervez <pervezhossain1998@gmail.com>
2023-07-13 12:06:58 +0600

    Hero Section Responsive Added

--- src/Components/Home/Hero/Hero.js
+++ src/Components/Home/Hero/Hero.js
@@ -15,12 +15,12 @@ const Hero = () => {
   return (
     <section className="container mx-auto">
       {/* Whole Container */}
-      <div className="mt-[50px] mb-[80px] grid lg:grid-cols-2 gap-[20px] items-center">
+      <div className="lg:mt-[50px] mb-[80px] p-[20px] grid lg:grid-cols-2 gap-[20px] items-center">
         <div>
-          <h1 className="md:text-[80px] font-bold leading-[80px]">We Make <span className="text-sky-600">Retouching</span> Easy</h1>
+          <h1 className="text-[60px] md:text-[80px] font-bold leading-[60px] lg:leading-[80px]">We Make <span className="text-sky-600">Retouching</span> Easy</h1>
           <p className="text-[18px] mt-[16px]">Photo Editing Solutions for Fashion, Apparel, E-commerce Businesses and Pro-Photographers. Our 100+ pro photo editors work 24/7 to provide you with the most personalized solutions in the market</p>
           <button className="tryUsFreeBtn">Try Us Free</button>
-          <p className="mt-[8px] text-[#667085] text-[14px]">No Credit Card Required</p>
+          <p className="mt-[8px] font-medium text-[#667085] text-[14px]">No Credit Card Required</p>
         </div>
         <div>
           <ReactBeforeSliderComponent
