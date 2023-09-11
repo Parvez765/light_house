c426a66e1db3d8474f154b385909b17ac96a2e28
parvez <pervezhossain1998@gmail.com>
2023-09-11 08:35:43 +0600

    Random Commit

---
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../../Components/HowItWorks/Hero';
+
+const HowItWorks = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    );
+};
+
+export default HowItWorks;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
 import HomePage from "../../Pages/HomePage/HomePage";
+import HowItWorks from "../../Pages/HowItWorks/HowItWorks";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <HomePage />,
       },
+      {
+        path: "/howitworks",
+        element : <HowItWorks/>
+      }
     ],
   },
 ]);
