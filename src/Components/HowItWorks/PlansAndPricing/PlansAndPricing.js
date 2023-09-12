651c6cea158b520bcc78d31a4d6194183c3389d2
tantaana <shariartanvirofficial@gmail.com>
2023-09-12 11:08:53 +0600

    plan has been added

--- src/Components/HowItWorks/PlansAndPricing/PlansAndPricing.js
+++ src/Components/HowItWorks/PlansAndPricing/PlansAndPricing.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
+import tickImg from "../../../Assets/PlanAndPricing/tick.png";
 
 const PlansAndPricing = () => {
-  const [count, setCount] = useState(1);
+  // All states
+  const [count1, setCount1] = useState(1);
+  const [count2, setCount2] = useState(1);
+  const [count3, setCount3] = useState(1);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  // All Increment Function
+  const handleIncrement1 = () => {
+    setCount1(count1 + 1);
+  };
+  const handleIncrement2 = () => {
+    setCount2(count2 + 1);
+  };
+  const handleIncrement3 = () => {
+    setCount3(count3 + 1);
   };
 
-  const handleDecrement = () => {
-    if (count > 1) {
-      setCount(count - 1);
+  // All Decrement Function
+  const handleDecrement1 = () => {
+    if (count1 > 1) {
+      setCount1(count1 - 1);
+    }
+  };
+  const handleDecrement2 = () => {
+    if (count2 > 1) {
+      setCount2(count2 - 1);
+    }
+  };
+  const handleDecrement3 = () => {
+    if (count3 > 1) {
+      setCount3(count3 - 1);
     }
   };
 
   return (
-    <div className="mx-4">
+    <div className="mx-4 my-4">
       <div className="mb-10">
         <h2 className="font-bold text-5xl text-center mb-6">Plans & Pricing</h2>
         <h2 className="text-[#595a5c] font-semibold text-lg text-center">
@@ -37,57 +59,300 @@ const PlansAndPricing = () => {
         </div>
       </div>
 
-      <h2 className="font-bold text-center text-[#595a5c]">
+      <h2 className="font-bold text-center text-[#595a5c] mb-8">
         <span className="text-green-600">Save 20%</span> For Annual Plan
       </h2>
 
       {/* Plan Cards */}
-      <div>
-        <div className="flex justify-center gap-6 mb-10">
-          <div className="flex flex-col">
-            <h2 className="text-4xl font-bold">Basic</h2>
-            <h2 className="text-[#595a5c] font-semibold">Full Time</h2>
+      <div className="flex justify-center items-center gap-6">
+        <div className="flex flex-col border border-gray-300 rounded-xl px-8 py-8 w-[420px]">
+          <div className="flex justify-between items-center gap-6 mb-10">
+            <div className="flex flex-col">
+              <h2 className="text-4xl font-bold">Basic</h2>
+              <h2 className="text-[#595a5c] font-semibold">Full Time</h2>
+            </div>
+
+            <div className="flex flex-col items-end">
+              <h2 className="text-4xl text-black font-bold">
+                $24
+                <span className="text-[#595a5c] text-lg font-semibold">
+                  /Day
+                </span>
+              </h2>
+
+              <h2 className="text-[#595a5c] font-semibold">
+                Billed monthly{" "}
+                <span className="text-black text-lg font-bold">$480</span>
+              </h2>
+            </div>
           </div>
 
-          <div className="flex flex-col items-end">
-            <h2 className="text-4xl text-black font-bold">
-              $24
-              <span className="text-[#595a5c] text-lg font-semibold">/Day</span>
+          <h2 className="text-[#595a5c] font-medium mb-16">
+            This package entails the most basic of your work. Your assigned
+            photo editor will do the primary work for your retouching needs.
+          </h2>
+
+          <div className="flex justify-center items-center gap-4 mb-10">
+            <h2 className="text-[#595a5c] text-lg font-semibold">
+              Add Team Member
             </h2>
 
-            <h2 className="text-[#595a5c] font-semibold">
-              Billed monthly{" "}
-              <span className="text-black text-lg font-bold">$480</span>
-            </h2>
+            <div className="flex justify-center items-center gap-1">
+              <h2
+                onClick={handleDecrement1}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                -
+              </h2>
+
+              <h2 className="text-[#595a5c] text-lg font-semibold text-center w-[30px]">
+                {count1}
+              </h2>
+
+              <h2
+                onClick={handleIncrement1}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                +
+              </h2>
+            </div>
+          </div>
+
+          <h2 className="text-sky-600 font-semibold text-center py-4 mb-4 rounded-lg hover:bg-sky-200 hover:cursor-pointer duration-300 ease-in-out">
+            Pay & Build Your Team Now!
+          </h2>
+
+          <div className="flex flex-col">
+            <h2 className="text-xl font-bold mb-6">Skills in Basic</h2>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Clipping Path
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Background Removal
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Shadow Creation
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Image Resizing
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Web Optimization
+              </h2>
+            </div>
           </div>
         </div>
 
-        <h2 className="text-[#595a5c] font-medium mb-16">
-          This package entails the most basic of your work. Your assigned photo
-          editor will do the primary work for your retouching needs.
-        </h2>
+        <div className="flex flex-col border border-gray-300 rounded-xl px-8 py-8 w-[420px]">
+          <div className="flex justify-between items-center gap-6 mb-10">
+            <div className="flex flex-col">
+              <h2 className="text-4xl font-bold">Basic</h2>
+              <h2 className="text-[#595a5c] font-semibold">Full Time</h2>
+            </div>
 
-        <div className="flex justify-center items-center">
-          <h2 className="text-[#595a5c] text-lg font-semibold">
-            Add Team Member
+            <div className="flex flex-col items-end">
+              <h2 className="text-4xl text-black font-bold">
+                $24
+                <span className="text-[#595a5c] text-lg font-semibold">
+                  /Day
+                </span>
+              </h2>
+
+              <h2 className="text-[#595a5c] font-semibold">
+                Billed monthly{" "}
+                <span className="text-black text-lg font-bold">$480</span>
+              </h2>
+            </div>
+          </div>
+
+          <h2 className="text-[#595a5c] font-medium mb-16">
+            This package entails the most basic of your work. Your assigned
+            photo editor will do the primary work for your retouching needs.
           </h2>
 
-          <div className="flex justify-center items-center gap-3">
-            <h2
-              onClick={handleDecrement}
-              className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
-            >
-              -
+          <div className="flex justify-center items-center gap-4 mb-10">
+            <h2 className="text-[#595a5c] text-lg font-semibold">
+              Add Team Member
             </h2>
 
-            <h2 className="text-[#595a5c] text-lg font-semibold">{count}</h2>
+            <div className="flex justify-center items-center gap-1">
+              <h2
+                onClick={handleDecrement2}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                -
+              </h2>
 
-            <h2
-              onClick={handleIncrement}
-              className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
-            >
-              +
+              <h2 className="text-[#595a5c] text-lg font-semibold text-center w-[30px]">
+                {count2}
+              </h2>
+
+              <h2
+                onClick={handleIncrement2}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                +
+              </h2>
+            </div>
+          </div>
+
+          <h2 className="text-sky-600 font-semibold text-center py-4 mb-4 rounded-lg hover:bg-sky-200 hover:cursor-pointer duration-300 ease-in-out">
+            Pay & Build Your Team Now!
+          </h2>
+
+          <div className="flex flex-col">
+            <h2 className="text-xl font-bold mb-6">Skills in Basic</h2>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Clipping Path
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Background Removal
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Shadow Creation
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Image Resizing
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Web Optimization
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col border border-gray-300 rounded-xl px-8 py-8 w-[420px]">
+          <div className="flex justify-between items-center gap-6 mb-10">
+            <div className="flex flex-col">
+              <h2 className="text-4xl font-bold">Basic</h2>
+              <h2 className="text-[#595a5c] font-semibold">Full Time</h2>
+            </div>
+
+            <div className="flex flex-col items-end">
+              <h2 className="text-4xl text-black font-bold">
+                $24
+                <span className="text-[#595a5c] text-lg font-semibold">
+                  /Day
+                </span>
+              </h2>
+
+              <h2 className="text-[#595a5c] font-semibold">
+                Billed monthly{" "}
+                <span className="text-black text-lg font-bold">$480</span>
+              </h2>
+            </div>
+          </div>
+
+          <h2 className="text-[#595a5c] font-medium mb-16">
+            This package entails the most basic of your work. Your assigned
+            photo editor will do the primary work for your retouching needs.
+          </h2>
+
+          <div className="flex justify-center items-center gap-4 mb-10">
+            <h2 className="text-[#595a5c] text-lg font-semibold">
+              Add Team Member
             </h2>
+
+            <div className="flex justify-center items-center gap-1">
+              <h2
+                onClick={handleDecrement3}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                -
+              </h2>
+
+              <h2 className="text-[#595a5c] text-lg font-semibold text-center w-[30px]">
+                {count3}
+              </h2>
+
+              <h2
+                onClick={handleIncrement3}
+                className="border border-gray-400 hover:border-sky-600 text-3xl text-black hover:text-sky-600 font-bold text-center rounded-lg w-[40px] h-[40px] duration-200 ease-in-out hover:cursor-pointer"
+              >
+                +
+              </h2>
+            </div>
+          </div>
+
+          <h2 className="text-sky-600 font-semibold text-center py-4 mb-4 rounded-lg hover:bg-sky-200 hover:cursor-pointer duration-300 ease-in-out">
+            Pay & Build Your Team Now!
+          </h2>
+
+          <div className="flex flex-col">
+            <h2 className="text-xl font-bold mb-6">Skills in Basic</h2>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Clipping Path
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Background Removal
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Shadow Creation
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Image Resizing
+              </h2>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <img src={tickImg} className="w-[40px]" alt="tick.png" />
+              <h2 className="text-lg font-medium text-[#595a5c]">
+                Web Optimization
+              </h2>
+            </div>
           </div>
         </div>
       </div>
