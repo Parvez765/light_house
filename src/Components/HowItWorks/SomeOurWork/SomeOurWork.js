c409c4789f2e4e1d941bcb6a9c114319227b692f
tantaana <shariartanvirofficial@gmail.com>
2023-09-13 13:13:58 +0600

    some of our work has been finished

--- src/Components/HowItWorks/SomeOurWork/SomeOurWork.js
+++ src/Components/HowItWorks/SomeOurWork/SomeOurWork.js
@@ -19,16 +19,36 @@ import img6 from "../../../Assets/SomeOurWork/img6.webp";
 import img7 from "../../../Assets/SomeOurWork/img7.webp";
 import img8 from "../../../Assets/SomeOurWork/img8.webp";
 
+import img9 from "../../../Assets/SomeOurWork/img9.webp";
+import img10 from "../../../Assets/SomeOurWork/img10.webp";
+import img11 from "../../../Assets/SomeOurWork/img11.webp";
+import img12 from "../../../Assets/SomeOurWork/img12.webp";
+import img13 from "../../../Assets/SomeOurWork/img13.webp";
+import img14 from "../../../Assets/SomeOurWork/img14.webp";
+import img15 from "../../../Assets/SomeOurWork/img15.webp";
+import img16 from "../../../Assets/SomeOurWork/img16.webp";
+
 const SomeOurWork = () => {
-  const someWorks = [
-    { img: img1 },
-    { img: img2 },
-    { img: img3 },
-    { img: img4 },
-    { img: img5 },
-    { img: img6 },
-    { img: img7 },
-    { img: img8 },
+  const slider1 = [
+    { slide1: img1 },
+    { slide1: img2 },
+    { slide1: img3 },
+    { slide1: img4 },
+    { slide1: img5 },
+    { slide1: img6 },
+    { slide1: img7 },
+    { slide1: img8 },
+  ];
+
+  const slider2 = [
+    { slide2: img9 },
+    { slide2: img10 },
+    { slide2: img11 },
+    { slide2: img12 },
+    { slide2: img13 },
+    { slide2: img14 },
+    { slide2: img15 },
+    { slide2: img16 },
   ];
 
   return (
@@ -45,6 +65,7 @@ const SomeOurWork = () => {
         </h2>
       </div>
 
+      {/* 1st slider */}
       <Swiper
         slidesPerView={1}
         spaceBetween={20}
@@ -67,21 +88,65 @@ const SomeOurWork = () => {
           delay: 500,
           disableOnInteraction: false,
         }}
-        pauseOnMouseEnter={true}
         speed={2000}
         modules={[Autoplay]}
         className={`mySwiper ${styles.mainDiv}`}
       >
-        {someWorks?.map((someWork, i) => {
+        {slider1?.map((someWork, i) => {
           return (
             <SwiperSlide
               key={i}
-              className="rounded-xl shadow-xl drop-shadow-2xl"
+              className="rounded-t-xl shadow-xl drop-shadow-2xl"
             >
               <div>
                 <img
-                  src={someWork?.img}
-                  className="h-[400px] w-full rounded-xl object-cover object-center"
+                  src={someWork?.slide1}
+                  className="h-[360px] w-full rounded-t-xl object-cover object-center"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      {/* 2nd slider */}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={20}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1280: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+          },
+          1536: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+        }}
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        speed={3000}
+        modules={[Autoplay]}
+        className={`mySwiper ${styles.mainDiv}`}
+      >
+        {slider2?.map((someWork, i) => {
+          return (
+            <SwiperSlide
+              key={i}
+              className="rounded-b-xl shadow-xl drop-shadow-2xl"
+            >
+              <div>
+                <img
+                  src={someWork?.slide2}
+                  className="h-[360px] w-full rounded-b-xl object-cover object-center"
                   alt=""
                 />
               </div>
