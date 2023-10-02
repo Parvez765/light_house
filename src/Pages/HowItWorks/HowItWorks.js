cf57da46319b9ea16ba1bc57eade946bdcff38e1
tantaana <shariartanvirofficial@gmail.com>
2023-10-02 23:11:14 +0600

    page system has been changed

--- src/Pages/HowItWorks/HowItWorks.js
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PlansAndPricing from "../../Components/HowItWorks/PlansAndPricing/PlansAndPricing";
 import SomeOurWork from "../../Components/HowItWorks/SomeOurWork/SomeOurWork";
+import AllTitle from "../../Hooks/AllTitle/AllTitle";
 
 const HowItWorks = () => {
+  AllTitle("How It Works");
   return (
     <div>
       <PlansAndPricing />
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main";
 import HomePage from "../../Pages/HomePage/HomePage";
 import HowItWorks from "../../Pages/HowItWorks/HowItWorks";
-import AboutUs from "../../Components/AboutUs/AboutUs";
+import AboutUs from "../../Pages/AboutUs/AboutUs";
 
 export const router = createBrowserRouter([
   {
